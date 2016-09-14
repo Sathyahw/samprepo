9540dcf46ff1f9ba10b2205976665fc885d8b9e1
H156368 <H156368@IE22LT8VWL6C2>
2016-09-14 15:11:20 +0530

    GIT

--- Documents/visual studio 2015/Projects/SampleApp/SampleApp/Program.cs
+++ Documents/visual studio 2015/Projects/SampleApp/SampleApp/Program.cs
@@ -10,7 +10,7 @@ namespace SampleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!!");
+            Console.WriteLine("Hello World!!! GIT");
             Console.ReadKey();
         }
     }
